454d18587e69aff1cfd6d8521ea6d1851f17f81f
jammacn <jeff@jamma.cn>
2019-12-19 22:19:59 +0800

    Dockerfile package.json

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 require('log4js').configure(require('path').join(__dirname, 'log4js.json'))
+process.env.NODE_CONFIG_DIR = require('path').join(__dirname)
 const config = require('config')
 if (process.env['disable_mq']) delete config.modules['jm-user-mq']
 module.exports = config
